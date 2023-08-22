b0de300f2b40f717c64eb7b838d8e016d8554f33
RadamantiumX <eduardo2502@gmail.com>
2023-08-22 18:08:43 -0300

    manejo de consultas en servidor y listado de respuesta

--- products.php
+++ products.php
@@ -6,8 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./styles/styles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
@@ -89,85 +88,105 @@
             <!-- dropdown search de marcas -->
             <div class="main">
                 <div class="select-container">
-                    
-                    <select id="select-brand" onchange="handleBrand()" name="Marca">
-                    <option selected>Seleccione una opcion</option>
+                   <form action="products.php" method="post" id="brand-form">
+                    <select onchange="document.getElementById('brand-form').submit()" id="select-brand" name="marca">
+                        <option selected>Seleccione una opcion</option>
                         <?php
-                         $sql_brand = "SELECT DISTINCT brand FROM catalogo";
-                         $b_results = mysqli_query($conn, $sql_brand);
-                         while($show_b = mysqli_fetch_assoc($b_results)){
+                        $sql_brand = "SELECT DISTINCT brand FROM catalogo";
+                        $b_results = mysqli_query($conn, $sql_brand);
+                        while ($show_b = mysqli_fetch_assoc($b_results)) {
 
-                         
+
                         ?>
-                        <option value="<?php echo $show_b['brand'] ?>"><?php echo $show_b['brand'] ?></option>
-                        
+                            <option value="<?php echo $show_b['brand'] ?>"><?php echo $show_b['brand'] ?></option>
+
                         <?php } ?>
                     </select>
-
+                     </form>
                     <!-- dropdown search de modelos -->
+                    <form action="products.php" method="post" id="model-form">
+                    <select onchange="document.getElementById('model-form').submit()" id="model-select"  name="modelo">
+                     <option selected>Modelo</option>
+                       <?php 
+                       if(isset($_POST['marca'])){
 
-                    <select id="model-select" name="Modelo">
-                        <option selected>Modelo</option>
-
-                        
-                        
-                        
+                        $brand = $_POST['marca'];
+                        $sql_model = "SELECT * FROM catalogo WHERE brand = '".$brand."'";
+                        $m_results = mysqli_query($conn,$sql_model);
+                        while($show_m=mysqli_fetch_assoc($m_results)){
+                       ?>
+                       
+                       <option value="<?php echo $show_m['catID'] ?>"><?php echo $show_m['model']  ?></option>
+                       <?php }} ?>
                     </select>
+                  </form>
+
+                    
+
+                    
+                    
+
                 </div>
             </div>
 
 
             <!-- Detalles del equipo -->
-
+           <?php
+           if(isset($_POST['modelo'])){
+              
+            $id = $_POST['modelo'];
+            $sql_id = "SELECT * FROM catalogo WHERE catID =".$id;
+            $i_results = mysqli_query($conn,$sql_id);
+            $product = mysqli_fetch_assoc($i_results);
+           ?>
             <div class="producto">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <!-- imagen del producto -->
-                <img style="width: 100%; max-width: 100%;" src="https://storage.googleapis.com/madebygoog/v1/phone/phone_hero-module_hero-image_1440_2x.png" alt="" />
-            </div>
-            <div class="col-md-8">
-                <h1>Smartphone Google Pixel 32 GB-Negro</h1>
-                <h3>Características</h3>
-                <ul>
-                    <li>Pantalla de 5 pulgadas.</li>
-                    <li>Procesador Snapdragon 821.</li>
-                    <li>Memoria de almacenamiento de 32 gb y espacio ilimitado en Google Cloud para fotos y videos 4k.</li>
-                    <li>Cámara principal de 12 MP con doble flash y frontal de 8 MP.</li>
-                    <li>Sensor de huellas Pixel Imprint y asistente personal Google Assistant.</li>
-                    <li>Carga de hasta 7 horas de duración en sólo 15 mins</li>
-                    <li>4GB Memoria RAM</li>
-                    <li>Android OS, v7.1 (Nougat)</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
-
-                  
-
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <!-- imagen del producto -->
+                            <img style="width: 100%; max-width: 100%;" src="<?php echo $product['image_path'] ?>" alt="Producto de alta Tecnologia, SmartPhones, Tablets, Notebooks" />
+                        </div>
+                        <div class="col-md-8">
+                            <h1><?php echo $product['brand']." | ".$product['model']  ?></h1>
+                            <h3>Características</h3>
+                            <ul>
+                                <li>Pantalla de <?php echo $product['display_size_and_technology'] ?></li>
+                                <li>Dimensiones:  <?php echo $product['Dimensions'] ?>.</li>
+                                <li>Memoria de almacenamiento de <?php echo $product['memory'] ?> y espacio ilimitado en Google Cloud para fotos y videos 4k.</li>
+                                <li>Cámara principal de <?php echo $product['rear_camera'] ?> con doble flash y frontal de <?php echo $product['front_camera'] ?>.</li>
+                                <li><?php echo $product['features'] ?></li>
+                                <li><?php echo $product['Lithium_Battery'] ?></li>
+                                <li><?php echo $product['RAM__DDR3_'] ?> Memoria RAM</li>
+                                <li>Android OS: <?php echo $product['android_version'] ?></li>
+                                <li><a href="<?php echo $product['Product_URL'] ?>" target="new">Link de compra</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
+            </div>
+             <?php } ?>
+
+
+        </div>
 
 
 
 
 
 
+        
+
+        <!--AXIOS-->
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
 
-                <!--AXIOS-->
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+        </script>
+        <script src="https://kit.fontawesome.com/39f24fdfe8.js" crossorigin="anonymous"></script>
+        <script src="./js/sidebar.js"></script>
+        <script src="./js/dropdown.js"></script>
 
-
-                <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-                    integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
-                    crossorigin="anonymous">
-                </script>
-                <script src="https://kit.fontawesome.com/39f24fdfe8.js" crossorigin="anonymous"></script>
-                <script src="./js/sidebar.js"></script>
-                <script src="./js/dropdown.js"></script>
-                <script src="./js/request.js"></script>
 
 
 </body>
